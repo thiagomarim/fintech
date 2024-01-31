93dec6fc555938a74d8920f205ae3e0c41172ef5
Thiago <thiago.marim2005@gmail.com>
2024-01-31 18:09:28 -0300

    Botão de meses adicionado

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import DateRange from "./DateRange";
+import Meses from "./Meses";
 
 const Header = () => {
-  return <DateRange />;
+  return (
+    <header className="mb">
+      <div className="mb">
+        <DateRange />
+      </div>
+      <Meses />
+    </header>
+  );
 };
 
 export default Header;
