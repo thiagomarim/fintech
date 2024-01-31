2d1e2123fe720963ab3fc91f27dc37ba30b484d3
Thiago <thiago.marim2005@gmail.com>
2024-01-31 18:44:21 -0300

    Adicionado o Rotas no SidNav

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./Style.css";
 import Header from "./components/Header";
 import Sidenav from "./components/Sidenav";
 import { DataContextProvider } from "./context/DataContext";
 import Resumo from "./pages/Resumo";
 import Vendas from "./pages/Vendas";
+import Venda from "./pages/Venda";
 
 function App() {
   return (
-    <DataContextProvider>
-      <div className="container">
-        <Sidenav />
-        <main>
-          <Header />
-          <Resumo />
-          <Vendas />
-        </main>
-      </div>
-    </DataContextProvider>
+    <BrowserRouter>
+      <DataContextProvider>
+        <div className="container">
+          <Sidenav />
+          <main>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Resumo />} />
+              <Route path="/vendas" element={<Vendas />} />
+              <Route path="/vendas/:id" element={<Venda />} />
+            </Routes>
+          </main>
+        </div>
+      </DataContextProvider>
+    </BrowserRouter>
   );
 }
 
