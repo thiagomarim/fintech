43bdc6972b1da6bb19b610c2d21d8f87496f19b5
Thiago <thiago.marim2005@gmail.com>
2024-01-31 18:51:06 -0300

    Adicionado cada informação da venda

--- src/pages/Venda.tsx
+++ src/pages/Venda.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+import { IVenda } from "../context/DataContext";
+import Loading from "../components/Loading";
+
+type VendaSemData = Omit<IVenda, "data">;
 
 const Venda = () => {
-  return <div>Venda</div>;
+  const { id } = useParams();
+  const { data, loading } = useFetch<VendaSemData>(
+    `https://data.origamid.dev/vendas/${id}`
+  );
+
+  if (loading === true) return <Loading />;
+  if (data === null) return null;
+  return (
+    <div>
+      <div className="box mb">ID: {data.id}</div>
+      <div className="box mb">Nome: {data.nome}</div>
+      <div className="box mb">
+        Preço:{" "}
+        {data.preco.toLocaleString("pt-br", {
+          style: "currency",
+          currency: "BRL",
+        })}
+      </div>
+      <div className="box mb">Status: {data.status}</div>
+      <div className="box mb">Pagamento: {data.pagamento}</div>
+    </div>
+  );
 };
 
 export default Venda;
